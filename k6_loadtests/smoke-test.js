6b973e9b8fe8b54f2c739d0fe3139b90ea138589
Snoozy <snoozy1995@gmail.com>
2022-04-29 22:55:39 +0200

    .

--- k6_loadtests/smoke-test.js
+++ k6_loadtests/smoke-test.js
@@ -8,6 +8,7 @@ import http from 'k6/http';
     http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
   },
 };*/
+/*
 export let options = {
   stages: [
       // Ramp-up from 1 to 5 virtual users (VUs) in 5s
@@ -25,6 +26,23 @@ export default function () {
   const response=http.get('http://185.196.21.189:3090/');
   check(response, { "status is 200": (r) => r.status == 200 });
   sleep(.300);
-}
+}*/
+
+import http from 'k6/http';
+import { check } from "k6";
+export let options = {
+  stages: [
+      // Ramp-up from 1 to 5 VUs in 5s
+      { duration: "5s", target: 5 },
+      // Stay at rest on 5 VUs for 10s
+      { duration: "10s", target: 5 },
+      // Ramp-down from 5 to 0 VUs for 5s
+      { duration: "5s", target: 0 }
+  ]
+};
+export default function () {
+  const response = http.get("https://swapi.dev/api/people/30/", {headers: {Accepts: "application/json"}});
+  check(response, { "status is 200": (r) => r.status === 200 });
+};
 
 //@todo https://k6.io/docs/test-types/smoke-testing/
