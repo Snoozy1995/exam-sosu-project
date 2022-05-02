16e14d8e0a7446ecb0caf7e3350d64cea07a93e4
Snoozy <snoozy1995@gmail.com>
2022-05-02 23:48:16 +0200

    Primevue

--- backend/src/enums/fs3Classifications.enum.ts
+++
@@ -1,8 +0,0 @@
-export enum FS3Diagram {
-  Henvendelse = 'henvendelse',
-  Funktionsevneområder = 'funktionsevneområder',
-  HelbredsOmråder = 'helbredsområder',
-  Indsats = 'indsats',
-  Opfølgning = 'opfølgning',
-  Persontilstand = 'persontilstand',
-}
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,11 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
 import Router from './router';
-
+import PrimeVue from 'primevue/config';
 const app=createApp(App);
 
 app.use(createPinia())
 app.use(Router);
-
+app.use(PrimeVue);
 
 app.mount('#app');
