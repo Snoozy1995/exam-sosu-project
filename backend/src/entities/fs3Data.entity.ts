d8c40f4dada5c77f27a2a119b6b1bc9a9c123d59
Snoozy <snoozy1995@gmail.com>
2022-05-02 02:17:50 +0200

    work

---
+++ backend/src/entities/fs3Data.entity.ts
@@ -0,0 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { Activity } from './activity.entity';
+import { Citizen } from './citizen.entity';
+import { FS3 } from './fs3.entity';
+@Entity()
+export class FS3Data {
+  @ApiProperty()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ApiProperty()
+  @ManyToOne(() => FS3)
+  fs3: FS3;
+
+  @ManyToOne(() => Citizen, (citizen) => citizen.fs3)
+  citizen: Citizen;
+
+  @ManyToOne(() => Activity, (activity) => activity.fs3)
+  activity: Activity;
+
+  @ApiProperty()
+  @Column()
+  input: string;
+
+  @ApiProperty()
+  @Column({ nullable: true })
+  functionalAbility: number;
+  //
+}
