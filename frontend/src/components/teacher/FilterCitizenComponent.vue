3a72aa7b43663f444dc3b6ea05612960a4a5cbe7
m1stertt <52206366+m1stertt@users.noreply.github.com>
2022-05-09 18:17:15 +0200

    Filter updated

--- frontend/src/components/teacher/FilterCitizenComponent.vue
+++ frontend/src/components/teacher/FilterCitizenComponent.vue
@@ -14,40 +14,33 @@ let citizens = ref<Citizen[]>([
 
 let selectedCitizen = ref<Citizen>();
 const citizenService = ref(new CitizenService());
-const filters = ref({
-  'firstName': {value: null, matchMode: EQUALS_FILTER.value},
-  'lastName': {value: null, matchMode: EQUALS_FILTER.value},
-});
-const filters2 = ref({
-  global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-  name: {
-    operator: FilterOperator.AND,
-    constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }],
-  },
-  "country.name": {
-    operator: FilterOperator.AND,
-    constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }],
-  },
-  representative: { value: null, matchMode: FilterMatchMode.IN },
-  date: {
-    operator: FilterOperator.AND,
-    constraints: [{ value: null, matchMode: FilterMatchMode.DATE_IS }],
-  },
-  balance: {
-    operator: FilterOperator.AND,
-    constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }],
-  },
-  status: {
-    operator: FilterOperator.OR,
-    constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }],
-  },
-  activity: { value: null, matchMode: FilterMatchMode.BETWEEN },
-  verified: { value: null, matchMode: FilterMatchMode.EQUALS },
-});
-const matchModeOptions = ref([
-  {label: 'Fornavn', value: FilterMatchMode.EQUALS},
 
-]);
+const filters = ref({
+  global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+  firstname: {
+    operator: FilterOperator.AND,
+    constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }],
+  },
+  firstName: {
+    constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }],
+    operator: FilterOperator.AND,
+  },
+  lastName: {
+    operator: FilterOperator.AND,
+    constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }],
+  },
+  birthday: {
+    operator: FilterOperator.AND,
+    constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }],
+  },
+
+});
+const matchModesOptions = [
+  {label: 'Matcher', value: FilterMatchMode.EQUALS},
+  {label: 'Matcher ikke', value: FilterMatchMode.NOT_EQUALS},
+  {label: 'Starter med', value: FilterMatchMode.STARTS_WITH},
+  {label: 'Ender med', value: FilterMatchMode.ENDS_WITH},
+]
 const loading = ref(true);
 
 
@@ -55,96 +48,74 @@ function create() { //@todo
 
 }
 
+FilterService.register(EQUALS_FILTER.value, (value, filter) => {
+  if (filter === undefined || filter === null || filter.trim() === '') {
+    return true;
+  }
+
+  if (value === undefined || value === null) {
+    return false;
+  }
+
+  return value.toString() === filter.toString();
+});
+
+
 onMounted(() => {
   citizenService.value.getAlltCitizens().then((result) => citizens.value = result.data as Citizen[])
       .catch((error) => console.log("error: " + error))
   loading.value = false;
   });
 
-  FilterService.register(EQUALS_FILTER.value, (value, filter) => {
-    if (filter === undefined || filter === null || filter.trim() === '') {
-      return true;
-    }
-
-    if (value === undefined || value === null) {
-      return false;
-    }
-
-    return value.toString() === filter.toString();
-  });
 
 
 
 </script>
 <template>
-<!--  <div>-->
-<!--    <h5>Left Icon</h5>-->
-<!--    <span class="p-input-icon-left ">-->
-<!--            <i class="pi pi-search"/>-->
-<!--            <InputText type="text" v-model="value3" placeholder="Search"/>-->
-<!--        </span>-->
-<!--  </div>-->
-<!--  <div class="pt-2 w-12rem" >-->
-<!--    <Listbox v-model="selectedItem" :options="items" optionLabel="label" optionValue="value" :virtualScrollerOptions="{ itemSize: 31 }" style="width:25rem" listStyle="height:250px" />-->
-<!--  </div>-->
-
-
   <div>
-    <div>
-      <div class="card">
-        <DataTable :value="citizens" v-model:selection="selectedCitizen" selectionMode="single" :paginator="true" :rows="10" responsiveLayout="scroll"
-                   dataKey="id" v-model:filters="filters" filterDisplay="row" :loading="loading" class="p-datatable-sm">
-          <template #empty>
-            Ingen borgere fundet.
-          </template>
-          <template #loading>
-            Indlæser borger data.. Vent venligst.
-          </template>
-          <template #header>
-            <h3 class="m-0">Borgere</h3>
-          </template>
+    <DataTable :value="citizens" :paginator="true" :rows="10"
+               dataKey="id" :rowHover="true" v-model:selection="selectedCitizen" v-model:filters="filters" filterDisplay="menu" :loading="loading"
+               paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown" :rowsPerPageOptions="[10,25,50]"
+               currentPageReportTemplate="Viser {first} af {last} ud af {totalRecords} borgere"
+               responsiveLayout="scroll">
+      <template #header>
+        <div class="flex justify-content-left align-items-center">
+          <h4 class="m-0">Borgere</h4>
 
+        </div>
+      </template>
+      <template #empty>
+        Ingen borgere fundet.
+      </template>
+      <template #loading>
+        Indlæser borger data. Vent venligst.
+      </template>
+      <Column selectionMode="single" headerStyle="width: 3rem"></Column>
+      <Column field="firstName" header="Fornavn:" sortable style="min-width: 14rem" :filterMatchModeOptions="matchModesOptions">
+        <template #body="{data}">
+          {{data.firstName}}
+        </template>
+        <template #filter="{filterModel}">
+          <InputText type="text" v-model="filterModel.value" placeholder="Fornavn:"/>
+        </template>
+      </Column>
+      <Column field="lastName" header="Efternavn:" sortable style="min-width: 14rem" :filterMatchModeOptions="matchModesOptions">
+        <template #body="{data}">
+          {{data.lastName}}
+        </template>
+        <template #filter="{filterModel}">
+          <InputText type="text" v-model="filterModel.value" placeholder="Efternavn:"/>
+        </template>
+      </Column>
+      <Column field="birthday" header="Fødselsdag:" sortable style="min-width: 14rem" :filterMatchModeOptions="matchModesOptions">
+        <template #body="{data}">
+          {{data.birthday}}
+        </template>
+        <template #filter="{filterModel}">
+          <InputText type="text" v-model="filterModel.value" placeholder="Fødselsdato:"/>
+        </template>
+      </Column>
 
-<!--          <Column field="name" header="Name" sortable style="min-width: 14rem">-->
-<!--            <template #body="{data}">-->
-<!--              {{data.name}}-->
-<!--            </template>-->
-<!--            <template #filter="{filterModel}">-->
-<!--              <InputText type="text" v-model="filterModel.value" class="p-column-filter" placeholder="Search by name"/>-->
-<!--            </template>-->
-<!--          </Column>-->
-
-          <Column field="firstName" header="Name" sortable style="min-width: 14rem">
-            <template #body="{data}">
-              {{data.firstName}}
-            </template>
-            <template #filter="{filterModel}">
-              <InputText type="text" v-model="filterModel.value" class="p-column-filter" placeholder="Search by name"/>
-            </template>
-          </Column>
-
-          <Column field="firstName" header="Fornavn:" sortable :filterMatchModeOptions="matchModeOptions">
-            <template #body="{data}">
-              {{data.firstName}}
-            </template>
-            <template #filter="{filterModel,filterCallback}">
-              <InputText type="text" v-model="filterModel.value" @input="filterCallback()" class="p-column-filter" :placeholder="`Fornavn: ${filterModel.matchMode}`"/>
-            </template>
-          </Column>
-          <Column field="lastName" header="Efternavn:" sortable :filterMatchModeOptions="matchModeOptions" >
-            <template #body="{data}">
-              {{data.lastName}}
-            </template>
-            <template #filter="{filterModel,filterCallback}">
-              <InputText type="text" v-model="filterModel.value" @input="filterCallback()" class="p-column-filter" :placeholder="`Efternavn:`"/>
-            </template>
-          </Column>
-
-          <Column field="birthday" header="Fødselsdag:" sortable></Column>
-
-        </DataTable>
-      </div>
-    </div>
-
+    </DataTable>
   </div>
 </template>
