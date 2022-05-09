aa93098362663347f1beb9c89b8ee3868dd2f0be
Snoozy <snoozy1995@gmail.com>
2022-05-09 22:12:26 +0200

    Fileuploads

--- backend/src/upload/upload.module.ts
+++ backend/src/upload/upload.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
+import { UploadedDocumentRepository } from 'src/domain/borders/uploadedDocument.repository';
+import { SaveFilesInteractor } from 'src/domain/use_cases/upload/saveFiles.interactor';
+import { UploadedDocumentRepositoryAdapter } from 'src/infrastructure/typeorm/uploadedDocumentRepository.adapter';
 import { UploadController } from './upload.controller';
+
+const repo = 'UploadedDocumentRepository';
+const inject = { inject: [repo] };
 
 @Module({
   controllers: [UploadController],
-  providers: [],
+  providers: [
+    {
+      provide: repo,
+      useClass: UploadedDocumentRepositoryAdapter,
+    },
+    {
+      ...inject,
+      provide: 'SaveFiles',
+      useFactory: (uploadRepo: UploadedDocumentRepository) =>
+        new SaveFilesInteractor(uploadRepo),
+    },
+  ],
 })
 export class UploadModule {}
