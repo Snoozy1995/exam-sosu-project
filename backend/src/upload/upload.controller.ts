3cf0e390542ce2a34e5708ef8bcf9a315b9db96f
Snoozy <snoozy1995@gmail.com>
2022-05-09 09:50:24 +0200

    initial file upload

--- backend/src/upload/upload.controller.ts
+++ backend/src/upload/upload.controller.ts
@@ -7,6 +7,9 @@ import {
 } from '@nestjs/common';
 import { AnyFilesInterceptor } from '@nestjs/platform-express';
 import { ApiTags } from '@nestjs/swagger';
+import * as mime from 'mime';
+import { diskStorage } from 'multer';
+import * as path from 'path';
 import { AuthenticatedGuard } from '../auth/guards/authenticated.guard';
 
 @ApiTags('upload')
@@ -14,8 +17,44 @@ import { AuthenticatedGuard } from '../auth/guards/authenticated.guard';
 @UseGuards(AuthenticatedGuard)
 export class UploadController {
   @Post()
-  @UseInterceptors(AnyFilesInterceptor())
+  @UseInterceptors(
+    AnyFilesInterceptor({
+      storage: diskStorage({
+        destination: './uploads/',
+        filename: function (req, file, cb) {
+          cb(null, Date.now() + path.extname(file.originalname));
+        },
+      }),
+      fileFilter: function (req, file, callback) {
+        const ext = path.extname(file.originalname);
+        if (
+          ext !== '.png' &&
+          ext !== '.jpg' &&
+          ext !== '.gif' &&
+          ext !== '.jpeg' &&
+          ext !== '.pdf' &&
+          ext !== '.docx' &&
+          ext !== '.txt' &&
+          ext !== '.xlsx'
+        ) {
+          return callback(new Error('File type not allowed'), false);
+        }
+        callback(null, true);
+      },
+      limits: {
+        fileSize: 1024 * 1024 * 10,
+      },
+    }),
+  )
   uploadFile(@UploadedFiles() files: Array<Express.Multer.File>) {
     console.log(files);
+    const response = [];
+    files.forEach((file) => {
+      const fileReponse = {
+        filename: file.filename,
+      };
+      response.push(fileReponse);
+    });
+    return response;
   }
 }
