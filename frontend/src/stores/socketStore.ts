6a62b9409a97d6f4c3368378a2394162eef9117e
Snoozy <snoozy1995@gmail.com>
2022-05-09 11:19:03 +0200

    .

--- frontend/src/stores/socketStore.ts
+++ frontend/src/stores/socketStore.ts
@@ -1,9 +1,10 @@
+import axios from "axios";
 import { defineStore } from "pinia";
 import { io } from 'socket.io-client';
 export const SocketStore = defineStore({
   id: "socketStore",
   state: ()=>({
-    socket: io("http://localhost:3000")
+    socket: io(axios.defaults.baseURL+"")
   }),
   actions:{
     
